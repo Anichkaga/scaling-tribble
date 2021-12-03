e0f331c22eda11435c2d9874ea9d0f01180c259e
Anichkaga <anyaproskurnyak@gmail.com>
2021-12-03 23:53:42 +0200

    finished homework2

---
+++ hm2/script.js
@@ -0,0 +1,27 @@
+let userName = 'Anka';
+console.log (userName);
+
+let userAge = '19';
+console.log (userAge);
+
+let userSurname = 'Yakimtsova';
+console.log (userSurname);
+
+let newUser = 'Ivan';
+console.log (newUser);
+
+let shoppingList ='milk';
+console.log (shoppingList);
+
+let onlineUser ='David';
+console.log (onlineUser);
+
+let salaryWorker ='2000';
+console.log (salaryWorker);
+
+let cursorEvent ='click';
+console.log (cursorEvent);
+
+const greeting ='Hello World!';
+console.log ('greeting', greeting)
+
